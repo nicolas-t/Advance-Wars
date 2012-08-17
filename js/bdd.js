d5a204cf4785c81d1ac8cfed61d1cdaca3b83c97
nicolas-t <nicolas.turlais@gmail.com>
2012-08-17 12:04:04 +0200

    changement portée tir : pour des tests

--- js/bdd.js
+++ js/bdd.js
@@ -181,7 +181,7 @@ var BDD = {
 			munition : 50,
 			essence : 20,
 			deplacement : 10,
-			porteeTir : {min : 0, max : 1},
+			porteeTir : {min : 2, max : 4},
 			vue : 2,
 			attaque : {infanterie : 75, tank : 50, bateau : 40, helico : 20}
 		}
