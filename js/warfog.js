f9ceb6f73767bf1d4b2994916cdbe474d4eadd45
nicolas-t <nicolas.turlais@gmail.com>
2012-08-19 22:52:11 +0200

    warfog instancié dans game

--- js/warfog.js
+++ js/warfog.js
@@ -4,28 +4,7 @@
 	this.warfogTotal = '';
 	if (typeof Warfog.initialized == "undefined" ) {
 
-		
-		Warfog.prototype.complet = function() {
-			var imgd = game.context.getImageData(0, 0, 256, 176);
-			var pix = imgd.data;
-			for (var i = 0, n = pix.length; i < n; i += 4) {
-				pix[i ] = pix[i]-110; 
-				pix[i+1] = pix[i+1]-100; 	
-				pix[i+2] = pix[i+2]-60; 	
-			}
-			game.context.putImageData(imgd, 0, 0);
-			this.warfogTotal = $.extend(true, {}, game.context.getImageData(0, 0, 256, 176));
-		}
-		Warfog.prototype.debrouille = function(coord) {
-			var imgd = game.context.getImageData(16*coord[0], 16*coord[1], 16, 16);
-			var pix = imgd.data;
-			for (var i = 0, n = pix.length; i < n; i += 4) {
-				pix[i ] = pix[i]+110; 
-				pix[i+1] = pix[i+1]+100; 	
-				pix[i+2] = pix[i+2]+60; 	
-			}
-			game.context.putImageData(imgd, 16*coord[0], 16*coord[1]);
-		}
+
 		Warfog.prototype.getAdversairesVisibles = function() {
 			for(var i=0; i < teams.length; i++){
 					for(var j=0; j < teams[i].units.length; j++){
@@ -40,14 +19,15 @@
 			}
 		}
 		Warfog.prototype.recalcul = function() {
-			game.context.putImageData(this.warfogTotal, 0, 0);
+			console.log(game.warfogTotal);
+			game.context.putImageData(game.warfogTotal, 0, 0);
 			this.afficherVue();
 		}
 		Warfog.prototype.afficherVue = function() {
 			this.getVue();
 			this.getAdversairesVisibles();
 			for(var i=0; i < this.vue.length; i++){
-				this.debrouille(getXY(this.vue[i]));
+				game.debrouilleWarfog(getXY(this.vue[i]));
 			}
 		}	
 		Warfog.prototype.calculPorteeVue = function(newX, newY, k, lim) {
