4d8f8a5b90f538207814ba1e9d354b0777ddf0e9
nicolas-t <nicolas.turlais@gmail.com>
2012-08-26 00:03:32 +0200

    class reflresh, pour mode synch
    communique avec bdd via ajax.

---
+++ js/refresh.class.js
@@ -0,0 +1,85 @@
+﻿function Refresh(duree) {
+	this.duree = duree;
+	var that = this;
+
+	setInterval(function (){
+		if(myTeam == controller.whosPlaying){
+			that.push();
+		}
+		else{
+			that.get();
+		} 
+	}, this.duree);
+	
+	if ( typeof Refresh.initialized == "undefined" ) {
+		Refresh.prototype.changeJoueur = function(joueur) {
+			$.ajax({
+				type: "POST",
+				url: "php/changeJoueur.php",
+				data: { 'j': joueur },
+			});
+			this.push();
+			controller.whosPlaying = myTeamInverse[myTeam];
+		}
+	
+		Refresh.prototype.push = function() {
+			var seen = [];
+			var lol = JSON.stringify(teams, function(key, val) {
+				if(key == "elem" || key == "attaque" || key == "c_avancement"){
+					return undefined;
+				}
+			   if (typeof val == "object") {
+					if (seen.indexOf(val) >= 0){ // doesn't work with chrome ?
+						return undefined}
+					seen.push(val);
+				}
+				return val;
+			});
+			
+			$.ajax({
+				type: "POST",
+				url: "php/push.php",
+				data: { 'json': lol }
+			});
+		}
+
+		Refresh.prototype.get = function() {
+			$.ajax({
+				type: "POST",
+				url: "php/get.php",
+				dataType: "json"
+			}).done(function(r) { 
+				if(r){
+					that.update($.parseJSON(r.json));
+					if(r.id_joueur == myTeam){
+						teams[myTeam].debutJour();
+						controller.whosPlaying = myTeam;
+					}
+				}
+			});
+		}
+		// aucune gestion des unites mortes.
+		Refresh.prototype.update = function(retour) {
+			for(i = 0; i<retour.length; i++)
+			{
+				for(j = 0; j<retour[i].units.length; j++)
+				{	
+					id = retour[i].units[j].id;
+					if(units[id] != 'undefined'){
+						delete unitsMap[units[id].x+'_'+units[id].y];
+						$.extend(true, units[id], retour[i].units[j]);
+						controller.warfog.getAdversairesVisibles();
+						units[id].updatePosition([retour[i].units[j].x, retour[i].units[j].y]);
+						units[id].updatePositionVisuelle([retour[i].units[j].x, retour[i].units[j].y]);
+						units[id].updateVie(0);
+					}
+					else{// c'est une nouvelle unité, il faut la créer
+						
+					}					
+				}
+			}
+
+		}
+		Refresh.initialized = true;
+	}
+}
